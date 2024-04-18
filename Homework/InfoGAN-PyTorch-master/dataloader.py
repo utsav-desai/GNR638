a56a911f9f3044ad00761dd575c7b7cd359e5512
Utsav Desai <utsavmdesai@gmail.com>
2024-04-18 16:32:08 +0530

    Edited dataloader.py

--- Homework/InfoGAN-PyTorch-master/dataloader.py
+++ Homework/InfoGAN-PyTorch-master/dataloader.py
@@ -1,57 +1,43 @@
 import torch
-import torchvision.transforms as transforms
-import torchvision.datasets as dsets
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+import os
 
-# Directory containing the data.
-root = 'data/'
+from datasets import load_dataset
+PatternNet = load_dataset("blanchon/PatternNet")
+
+
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, dataset, transform=None):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        image = self.dataset[idx]['image']
+        label = self.dataset[idx]['label']
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
 
 def get_data(dataset, batch_size):
 
-    # Get MNIST dataset.
-    if dataset == 'MNIST':
-        transform = transforms.Compose([
-            transforms.Resize(28),
-            transforms.CenterCrop(28),
-            transforms.ToTensor()])
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor()
+    ])
 
-        dataset = dsets.MNIST(root+'mnist/', train='train', 
-                                download=True, transform=transform)
+    dataset = CustomImageDataset(PatternNet['train'], transform)
 
-    # Get SVHN dataset.
-    elif dataset == 'SVHN':
-        transform = transforms.Compose([
-            transforms.Resize(32),
-            transforms.CenterCrop(32),
-            transforms.ToTensor()])
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-        dataset = dsets.SVHN(root+'svhn/', split='train', 
-                                download=True, transform=transform)
-
-    # Get FashionMNIST dataset.
-    elif dataset == 'FashionMNIST':
-        transform = transforms.Compose([
-            transforms.Resize(28),
-            transforms.CenterCrop(28),
-            transforms.ToTensor()])
-
-        dataset = dsets.FashionMNIST(root+'fashionmnist/', train='train', 
-                                download=True, transform=transform)
-
-    # Get CelebA dataset.
-    # MUST ALREADY BE DOWNLOADED IN THE APPROPRIATE DIRECTOR DEFINED BY ROOT PATH!
-    elif dataset == 'CelebA':
-        transform = transforms.Compose([
-            transforms.Resize(32),
-            transforms.CenterCrop(32),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5),
-                (0.5, 0.5, 0.5))])
-
-        dataset = dsets.ImageFolder(root=root+'celeba/', transform=transform)
-
-    # Create dataloader.
-    dataloader = torch.utils.data.DataLoader(dataset, 
-                                            batch_size=batch_size, 
-                                            shuffle=True)
-
-    return dataloader
+    return train_dataloader
